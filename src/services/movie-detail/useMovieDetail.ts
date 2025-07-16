e320e77fb711697d1b4952956e1fefc0b85c2811
Haris <hari@pijar.com>
2025-07-17 00:02:37 +0700

    change title

--- src/services/movie-detail/useMovieDetail.ts
+++ src/services/movie-detail/useMovieDetail.ts
@@ -1,6 +1,8 @@
 import useAxios from "axios-hooks";
 import type { MovieDetail } from "./type";
 
+import '../axios'
+
 export const useMovieDetail = (id: string) => {
   return useAxios<MovieDetail>(`/movie/${id}`);
 }
